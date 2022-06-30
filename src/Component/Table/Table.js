6cddd08854d6e5120f9fac25ed82f9c28f44a316
shakilkhanSAU <shakilkhan.sau@gmail.com>
2022-06-30 16:56:41 +0600

    commit

---
+++ src/Component/Table/Table.js
@@ -0,0 +1,61 @@
+import React, { useMemo } from "react";
+import { useTable } from "react-table";
+import Loader from '../Loader'
+import styles from "./styles.css";
+
+
+
+const Table = ({ columns, data, isLoading, manualPagination = false }) => {
+    const columnData = useMemo(() => columns, [columns]);
+    const rowData = useMemo(() => data, [data]);
+    const {
+        getTableProps,
+        getTableBodyProps,
+        headerGroups,
+        rows,
+        prepareRow,
+    } = useTable({
+        columns: columnData,
+        data: rowData,
+        manualPagination,
+    });
+    return (
+        <>
+            {isLoading ? (
+                <Loader></Loader>
+            ) : (
+                <>
+                    <table {...getTableProps()}>
+                        <thead>
+                            {headerGroups.map((headerGroup) => (
+                                <tr {...headerGroup.getHeaderGroupProps()}>
+                                    {headerGroup.headers.map((column) => (
+                                        <th {...column.getHeaderProps()}>
+                                            {column.render("Header")}
+                                        </th>
+                                    ))}
+                                </tr>
+                            ))}
+                        </thead>
+                        <tbody {...getTableBodyProps()}>
+                            {rows.map((row, i) => {
+                                prepareRow(row);
+                                return (
+                                    <tr {...row.getRowProps()}>
+                                        {row.cells.map((cell) => {
+                                            return (
+                                                <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                                            );
+                                        })}
+                                    </tr>
+                                );
+                            })}
+                        </tbody>
+                    </table>
+                </>
+            )}
+        </>
+    );
+};
+
+export default Table;
